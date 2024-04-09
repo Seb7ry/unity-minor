61a20fa2b1d3b07d8cff96081def2da0318ed74b
Juan Sebastian Murillo <sebasty06@outlook.com>
2024-04-08 23:29:34 -0500

    Add dash and delete palette.

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
-    private Animator Animator;
+
+    [Header("Movimiento")]
     private float horizontal;
     private bool grounded;
     public float speed;
     public float jumpForce;
 
+    [Header("Animacion")]
+    private Animator Animator;
+
+    [Header("Dash")]
+    [SerializeField] private float speedDash;
+    [SerializeField] private float timeDash;
+    private float gravedadInicial;
+    private bool puedeDash = true;
+    private bool puedeMover = true;
+
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        Animator = GetComponent<Animator>();  
+        Animator = GetComponent<Animator>(); 
+        gravedadInicial = Rigidbody2D.gravityScale;
     }
 
     void Update()
@@ -27,7 +39,6 @@ public class player_movement : MonoBehaviour
         Animator.SetBool("running", horizontal != 0.0f);
         
         //Preguntar
-        Debug.DrawRay(transform.position, Vector3.down*0.1f, Color.red);
         if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
             grounded = true;
@@ -38,15 +49,38 @@ public class player_movement : MonoBehaviour
         {
             Jump();
         }
+
+        if(Input.GetKeyDown(KeyCode.C) && puedeDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(horizontal * speed, Rigidbody2D.velocity.y);
+        if(puedeMover)
+        {
+            Rigidbody2D.velocity = new Vector2(horizontal * speed, Rigidbody2D.velocity.y);
+        }
     }
 
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * jumpForce);
+    }
+
+    private IEnumerator Dash()
+    {
+        puedeMover = false;
+        puedeDash = false;
+        Rigidbody2D.gravityScale = 0;
+        Rigidbody2D.velocity = new Vector2(speedDash * transform.localScale.x,0);
+        Animator.SetTrigger("dash");
+
+        yield return new WaitForSeconds(timeDash);
+
+        puedeMover = true;
+        puedeDash = true;
+        Rigidbody2D.gravityScale = gravedadInicial;
     }
 }
