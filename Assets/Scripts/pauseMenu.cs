dff6465155155f18107f2bcd85938bc5b912107f
Sebastian Murillo <sebasty06@outlook.com>
2024-05-10 11:46:15 -0500

    Main menu completed, pause menu in progress. Problems in the charge of a new scene, some object starts static.

---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    void Start()
+    {
+        juegoPausado = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(juegoPausado){
+                Reanudar();
+            } else 
+            {
+                Pausa();
+            }
+        }
+    }
+
+    public void Pausa(){
+        juegoPausado = true;
+        Time.timeScale = 0f;
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar(){
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar(){
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void VolverMenu(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit(){
+        Debug.Log("Cerrando");
+        Application.Quit();
+    }
+}
