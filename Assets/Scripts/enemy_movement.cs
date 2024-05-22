666e801f7984f13cd5e7dce7d1325ad53dfe8e3b
Juan David Gonzalez <juandavidgonzalez1@hotmail.com>
2024-05-22 13:49:00 -0500

    some aditions

--- Assets/Scripts/enemy_movement.cs
+++ Assets/Scripts/enemy_movement.cs
@@ -23,6 +23,8 @@ public class enemy_movement : MonoBehaviour
     public GameObject rango;
     public GameObject hit;
 
+    private bool isDead; // Nueva variable para verificar si está muerto
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -33,73 +35,77 @@ public class enemy_movement : MonoBehaviour
 
     void Update()
     {
-        Comportamientos();
+        if (!isDead) // Solo actualiza si no está muerto
+        {
+            Comportamientos();
+        }
     }
-
 
     public void Comportamientos()
     {
-        if(Mathf.Abs(transform.position.x - target.transform.position.x) > rangoVision && !atack)
-        {   
+        if (Mathf.Abs(transform.position.x - target.transform.position.x) > rangoVision && !atack)
+        {
             animator.SetBool("run", false);
             cronometro += 1 * Time.deltaTime;
-            if(cronometro >= 4)
+            if (cronometro >= 4)
             {
-                rutina = Random.Range(0,2);
+                rutina = Random.Range(0, 2);
                 cronometro = 0;
             }
 
-            switch(rutina)
+            switch (rutina)
             {
                 case 0:
                     //animator.SetBool("idle", false);
                     break;
-            
+
                 case 1:
-                    direccion = Random.Range(0,2);
+                    direccion = Random.Range(0, 2);
                     rutina++;
                     break;
                 case 2:
-                    switch(direccion)
+                    switch (direccion)
                     {
                         case 0:
-                            transform.rotation = Quaternion.Euler(0,0,0);
+                            transform.rotation = Quaternion.Euler(0, 0, 0);
                             transform.Translate(Vector3.right * speedRun * Time.deltaTime);
                             break;
                         case 1:
-                            transform.rotation = Quaternion.Euler(0,180,0);
+                            transform.rotation = Quaternion.Euler(0, 180, 0);
                             transform.Translate(Vector3.right * speedRun * Time.deltaTime);
                             break;
                     }
                     break;
             }
-        }else
+        }
+        else
         {
-            if(Mathf.Abs(transform.position.x - target.transform.position.x) > rangoAtaque && !atack)
+            if (Mathf.Abs(transform.position.x - target.transform.position.x) > rangoAtaque && !atack)
             {
-                if(transform.position.x < target.transform.position.x)
+                if (transform.position.x < target.transform.position.x)
                 {
                     transform.Translate(Vector3.right * 0.6f * Time.deltaTime);
-                    transform.rotation = Quaternion.Euler(0,0,0);
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
                     animator.SetBool("attack", false);
-                } else 
+                }
+                else
                 {
                     transform.Translate(Vector3.right * 0.6f * Time.deltaTime);
-                    transform.rotation = Quaternion.Euler(0,180,0);
+                    transform.rotation = Quaternion.Euler(0, 180, 0);
                     animator.SetBool("attack", false);
                 }
             }
             else
             {
-                if(!atack)
+                if (!atack)
                 {
-                    if(transform.position.x < target.transform.position.x)
+                    if (transform.position.x < target.transform.position.x)
                     {
-                        transform.rotation = Quaternion.Euler(0,0,0);
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
                     }
                     else
                     {
-                        transform.rotation = Quaternion.Euler(0,180,0);
+                        transform.rotation = Quaternion.Euler(0, 180, 0);
                     }
                     animator.SetBool("run", false);
                 }
@@ -107,23 +113,6 @@ public class enemy_movement : MonoBehaviour
         }
     }
 
-    void Patrol()
-    {
-        // Mover al enemigo dentro del rango de patrulla
-        if (transform.position.x >= patrolStartPosition && transform.position.x <= patrolEndPosition)
-        {
-            // Si está dentro del rango de patrulla, patrullar hacia adelante
-            transform.Translate(Vector3.right * speedRun * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else
-        {
-            // Si está fuera del rango de patrulla, invertir dirección de patrulla
-            speedRun *= -1;
-            transform.Rotate(Vector3.up * 180f);
-        }
-    }
-    
     public void FinalAni()
     {
         animator.SetBool("attack", false);
@@ -141,4 +130,13 @@ public class enemy_movement : MonoBehaviour
         hit.GetComponent<BoxCollider2D>().enabled = false;
     }
 
+    // Método para establecer el estado de muerto
+    public void SetDead()
+    {
+        isDead = true;
+        // Detén cualquier movimiento adicional
+        speedRun = 0;
+        animator.SetBool("run", false);
+        animator.SetBool("attack", false);
+    }
 }
